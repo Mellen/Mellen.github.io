45cc17e5165afcb0f945298786902f0af3318e0a
Matt Ellen <matt@matthewellen.co.uk>
2014-05-15 20:20:20 +0100

    initial commit for lon multiplication working shower

---
+++ showyourworking/working.js
@@ -0,0 +1,8 @@
+var btn = document.getElementById('btnMultiply');
+btn.addEventListener("click", startmultiply, false);
+
+function startmultiply()
+{
+    var n1 = document.getElementById('txtno1').value;
+    alert(n1);
+}
