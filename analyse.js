4a944c26bac21538d1fc4af6a1d934c04429f6d0
Matt Ellen <matt@matthewellen.co.uk>
2014-11-15 16:21:25 +0000

    started with patient/control barrier detection

--- analyse.js
+++ analyse.js
@@ -1,16 +1,19 @@
 var canvas = document.getElementById('canvas');
-var canvEdge = document.getElementById('edges');
+var ctx = canvas.getContext('2d');
+
+var canEdge = document.getElementById('edges');
+var ctxEdge = canEdge.getContext('2d');
+
 var vid = document.getElementById('vid');
 var dropPercent = document.getElementById('dp');
 var level = document.getElementById('level');
-
-var ctx = canvas.getContext('2d');
-var edgeCtx = canvEdge.getContext('2d');
 var localMediaStream = null;
 
-var red = 0;
-var green = 1;
-var blue = 2;
+//arbitrary values from one particular image used to produce ratios
+var fullWidth = 345;
+var patchWidthRatio = 143 / fullWidth;
+var midBarrierWidthRatio = 66 / fullWidth;
+var heightRatio = 86 / fullWidth;
 
 vid.addEventListener('click', capture, false);
 
@@ -41,25 +44,22 @@ function capture()
     {
 	canvas.width = vid.clientWidth;
 	canvas.height = vid.clientHeight;
-	canvEdge.width = vid.clientWidth;
-	canvEdge.height = vid.clientHeight;
+	canEdge.width = vid.clientWidth;
+	canEdge.height = vid.clientHeight;
 	ctx.drawImage(vid, 0, 0);
 	var pixels = ctx.getImageData(0,0, canvas.width, canvas.height);
-	var redPixels = edgeCtx.createImageData(canvEdge.width, canvEdge.height);
-	getColour(pixels, redPixels, red);
+	var edges = ctxEdge.createImageData(canEdge.width, canEdge.height);
+	setEdges(edges, pixels);
+	edgeIndices = getEdgeIndices(edges);
+
+	var bounds = calculateBounds(edgeIndices, canvas.width, canvas.height);
+
+	ctxEdge.putImageData(edges, 0, 0);
+
 	var ratios = calculateRatios(pixels);
 	console.log(ratios);
 	dropPercent.innerHTML = ratios.normalised_drop_percent;
 	level.innerHTML = ratios.level;
-    }
-}
-
-function getColour(pixels, colourField, colour)
-{    
-    for(var pi = 0; pi < pixels.data.length; pi += 4)
-    {
-	colourField.data[pi+colour] = pixels.data[pi+colour];
-	colourField.data[pi+3] = 255;
     }
 }
 
@@ -111,7 +111,7 @@ function calculateRatios(colourField)
     if(edgeIndices.length < 4)
     {
 	console.log('not enough edge');
-	return {};
+	return {normalised_drop_percent: 'none', level: 'none'};
     }
 
     var control = getDrop(edgeIndices[0] + 5, edgeIndices[1] - 5, normAvgs)
@@ -119,6 +119,9 @@ function calculateRatios(colourField)
     var patient = getDrop(Math.floor(edgeIndices[2] + 0.50 * (edgeIndices[3] - edgeIndices[2])), Math.floor(edgeIndices[2] + 0.80 * (edgeIndices[3] - edgeIndices[2])), normAvgs);
    
     var ratio = patient / control * 100
+
+    console.log('patient ' + patient);
+    console.log('control ' + control);
 
     return { 'normalised_drop_percent': ratio, 'level': Math.floor(ratio / 20.0) };
 }
@@ -139,4 +142,114 @@ function getDrop(x, y, normAvgs)
     var calculatedValueAtMinPosn = normAvgs[x] + proportionIntoControlStrip * controlStripHeight;
 
     return (calculatedValueAtMinPosn - minInControlStrip);
+}
+
+function setEdges(edges, colourField)
+{
+    var stdDev = 0;
+    var pixelCount = colourField.width * colourField.height;
+    var colourScore = 0;
+    for(var pi = 0; pi < colourField.data.length; pi += 4)
+    {
+	colourScore += colourField.data[pi];
+    }
+    var mean = colourScore / pixelCount;
+    var distsFromMeanSquared = [];
+
+    for(var pi = 0; pi < colourField.data.length; pi+=4)
+    {	
+	var redVal = colourField.data[pi];
+	distsFromMeanSquared.push((redVal - mean) * (redVal - mean));
+    }
+
+    stdDev = Math.sqrt(distsFromMeanSquared.reduce(function(a,b){return a+b;}) / distsFromMeanSquared.length);
+
+    var limit = 0.3;
+
+    for(var pi = 0; pi < edges.data.length; pi+=4)
+    {
+	if(pi % (colourField.width*4) != (colourField.width * 4))
+	{
+	    var redVal1 = colourField.data[pi];
+
+	    var redVal2 = colourField.data[pi+4];
+
+	    var scale = Math.abs(redVal1 - redVal2) / stdDev;
+	    if(scale > limit)
+	    {
+		edges.data[pi] = Math.floor(255 );
+		edges.data[pi+1] = Math.floor(255 );
+		edges.data[pi+2] = Math.floor(255 );
+	    }
+	}
+	edges.data[pi+3] = 255;
+    }
+
+    var width = edges.width * 4;
+
+    for(var pi = 0; pi < edges.data.length; pi+=4)
+    {
+	if(pi < (edges.data.length - (width)))
+	{
+	    var redVal1 = colourField.data[pi];
+
+	    var redVal2 = colourField.data[pi+width];
+
+	    var scale = Math.abs(redVal1 - redVal2) / stdDev;
+	    if(scale > limit)
+	    {
+		edges.data[pi] += Math.floor(255 );
+		edges.data[pi+1] += Math.floor(255 );
+		edges.data[pi+2] += Math.floor(255 );
+	    }
+	}
+	edges.data[pi+3] = 255;
+    }	
+}
+
+function getEdgeIndices(edges)
+{
+    var indices = [];
+    for(var i = 0; i < edges.data.length; i++)
+    {
+	if(edges.data[i] == 255)
+	{
+	    indices.push(i);
+	}
+    }
+}
+
+function calculateBounds(indices, width, height)
+{
+    var epsilon = 0.01;
+
+    var widths = [];
+
+    var heights = [];
+
+    for(var i = 0; i < indices.length-1; i++)
+    {
+	if(Math.floor(indices[i] / width) == Math.floor(indices[i+1] / width))
+	{
+	    var y = Math.floor(indices[i] / width);
+	    widths.push({dist:Math.abs(indices[i] - indices[i+1]), 
+			 x1: indices[i]%width, 
+			 x2: indices[i+1]%width, 
+			 y1: y, 
+			 y2: y});
+	}
+    }
+
+    for(var i = 0; i < indices.length - width; i++)
+    {
+	if((indices[i] % width) == (indices[i + width] % width))
+	{
+	    var x = indices[i] % width;
+	    heights.push({dist:Math.abs(indices[i] - indices[i+width]), 
+		          x1: x,
+			  x2: x, 
+			  y1: Math.floor(indices[i]/width), 
+			  y2: Math.floor(indices[i+width] / width)}));
+	}
+    }
 }
