85ca7f65a3faca8528acce4e794acf847165aa7e
Matt Ellen <matt@matthewellen.co.uk>
2015-08-16 23:51:36 +0100

    pixelated the 8bit image

--- EightBitify.me/8bit.js
+++ EightBitify.me/8bit.js
@@ -1,62 +1,116 @@
-var canvas = document.getElementById('can');
-var ctx = canvas.getContext('2d');
-var localMediaStream = null;
-
-vid.addEventListener('click', capture, false);
-
-navigator.getUserMedia = ( navigator.getUserMedia ||
-                       navigator.webkitGetUserMedia ||
-                       navigator.mozGetUserMedia ||
-                       navigator.msGetUserMedia);
-
-if(navigator.getUserMedia)
+(function()
 {
-    var um = navigator.getUserMedia({video: true}, handleVid, vidErr);
-}
+    "use strict";
+    var canvas = document.getElementById('can');
+    var ctx = canvas.getContext('2d');
+    var localMediaStream = null;
 
-function handleVid(stream)
-{
-    vid.src = window.URL.createObjectURL(stream);
-    localMediaStream = stream;
-}
+    vid.addEventListener('click', capture, false);
 
-function vidErr(e)
-{
-    alert(e);
-}
+    navigator.getUserMedia = ( navigator.getUserMedia ||
+			       navigator.webkitGetUserMedia ||
+			       navigator.mozGetUserMedia ||
+			       navigator.msGetUserMedia);
 
-function capture()
-{
-    if(localMediaStream)
+
+    if(navigator.getUserMedia)
     {
-	canvas.width = vid.clientWidth;
-	canvas.height = vid.clientHeight;
-	ctx.drawImage(vid, 0, 0);
-	make8bit();
+	var um = navigator.getUserMedia({video: true}, handleVid, vidErr);
     }
-}
 
-
-function make8bit()
-{
-    var image = ctx.getImageData(0,0,canvas.width, canvas.height);
-    for(var dp = 0; dp < image.data.length; dp += 4)
+    function handleVid(stream)
     {
-	var red = image.data[dp];
-	var green = image.data[dp+1];
-	var blue = image.data[dp+2];
+	vid.src = window.URL.createObjectURL(stream);
+	localMediaStream = stream;
+    }
 
-	var rng = 32;
-	var b = 64;
+    function vidErr(e)
+    {
+	alert(e);
+    }
+
+    function capture()
+    {
+	if(localMediaStream)
+	{
+	    canvas.width = vid.clientWidth;
+	    canvas.height = vid.clientHeight;
+	    ctx.drawImage(vid, 0, 0);
+	    make8bit();
+	}
+    }
+
+    function make8bit()
+    {
+	var image = ctx.getImageData(0, 0, canvas.width, canvas.height);
+
+	for(let dp = 0; dp < image.data.length; dp += 4)
+	{
+	    let red = image.data[dp];
+	    let green = image.data[dp+1];
+	    let blue = image.data[dp+2];
+
+	    let rng = 32;
+	    let b = 64;
+	    
+	    let newRed = Math.floor(red / rng) * rng;
+	    let newGreen = Math.floor(green / rng) * rng;
+	    let newBlue = Math.floor(blue / b) * b;
+
+	    image.data[dp] = newRed;
+	    image.data[dp+1] = newGreen;
+	    image.data[dp+2] = newBlue;
+	}
+
+	var pixelWidth = 6;
+
+	var maxX = image.width/pixelWidth;
+	var maxY = image.height/pixelWidth;
+
+	var pixelCount = pixelWidth * pixelWidth;
+
+	for(let x = 0; x < maxX; x++)
+	{
+	    for(let y = 0; y < maxY; y++)
+	    {
+		let colourSumR = 0;
+		let colourSumG = 0;
+		let colourSumB = 0;
+
+		let currentIndex = (x*pixelWidth + y*pixelWidth*image.width) * 4;
+
+		for(let innerX = 0; innerX < pixelWidth; innerX++)
+		{
+		    for(let innerY = 0; innerY < pixelWidth; innerY++)
+		    {
+			let extraIndex = (innerX + innerY * image.width) * 4;
+			let totalIndex = currentIndex + extraIndex;
+			colourSumR += image.data[totalIndex];
+			colourSumG += image.data[totalIndex+1];
+			colourSumB += image.data[totalIndex+2];
+		    }
+		}
+
+		let avgR = Math.floor(colourSumR / pixelCount);
+		let avgG = Math.floor(colourSumG / pixelCount);
+		let avgB = Math.floor(colourSumB / pixelCount);
+
+		for(let innerX = 0; innerX < pixelWidth; innerX++)
+		{
+		    for(let innerY = 0; innerY < pixelWidth; innerY++)
+		    {
+			let extraIndex = (innerX + innerY * image.width) * 4;
+			let totalIndex = currentIndex + extraIndex;
+			image.data[totalIndex] = avgR;
+			image.data[totalIndex+1] = avgG;
+			image.data[totalIndex+2] = avgB;
+		    }
+		}	    
+	    }
+	}
 	
-	var newRed = Math.floor(red / rng) * rng;
-	var newGreen = Math.floor(green / rng) * rng;
-	var newBlue = Math.floor(blue / b) * b;
+	console.log('length: ' + image.data.length + ', width: ' + image.width + ', height: ' + image.width);
 
-	image.data[dp] = newRed;
-	image.data[dp+1] = newGreen;
-	image.data[dp+2] = newBlue;
+	ctx.putImageData(image, 0, 0);
     }
-
-    ctx.putImageData(image, 0, 0);
-}
+})();
