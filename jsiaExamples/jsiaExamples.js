895c79eb17ef26fa472961afc18b319a0e644da8
Matt Ellen <matt@matthewellen.co.uk>
2015-10-28 17:06:42 +0000

    jsiaExamples initial commit

---
+++ jsiaExamples/jsiaExamples.js
@@ -0,0 +1,54 @@
+(function()
+ {
+     var img = document.getElementById('jsiaImg');
+
+     var canvas = document.getElementById('canv');
+     canvas.width = img.width;
+     canvas.height = img.height*3;
+     var context = canvas.getContext('2d');
+
+     var coords = document.getElementById('coords');
+
+     var lastCentre = null;
+
+     canvas.addEventListener('mousemove', function(e)
+			   {
+			       if(lastCentre != null)
+			       {
+				   invert9Pixels(lastCentre);
+			       }
+
+			       lastCentre = {x: e.offsetX, y: e.offsetY};
+
+			       invert9Pixels(lastCentre);
+			   });
+
+     function invert9Pixels(centre)
+     {
+	 var data = context.getImageData(centre.x - 7, centre.y - 7, 15, 15);
+	 for(var i = 0; i < data.data.length; i += 4)
+	 {
+	     var r = data.data[i];
+	     var g = data.data[i+1];
+	     var b = data.data[i+2];
+
+	     var inv = jsia.invertPixelColour(r, g, b, 255, false);
+
+	     data.data[i] = inv.r;
+	     data.data[i+1] = inv.g;
+	     data.data[i+2] = inv.b;
+	 }
+	 context.putImageData(data, centre.x - 7, centre.y - 7);
+     }
+
+     var data = jsia.getImageDataFromImg(img);
+
+     var greyData = jsia.imageDataToGreyScale(data, true);
+     context.putImageData(greyData, 0, 0);
+
+     var greyDarkData = jsia.imageDataToGreyScale(data, false);
+     context.putImageData(greyDarkData, 0, img.height);
+
+     var invertedData = jsia.imageDataInvertedColour(data, false);
+     context.putImageData(invertedData, 0, img.height*2);
+}());
