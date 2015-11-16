b1ba43d3c8544112696e22f1ea71b50442e77ff6
Matt Ellen <matt@matthewellen.co.uk>
2015-11-16 13:09:45 +0000

    finding the barcode a lot of the time

--- jsiaExamples/barcodeReader/barcode.js
+++ jsiaExamples/barcodeReader/barcode.js
@@ -2,10 +2,12 @@
  {
      var canvas = document.getElementById('canvas');
      var ctx = canvas.getContext('2d');
+     var canvas2 = document.getElementById('canvas2');
+     var ctx2 = canvas2.getContext('2d');
 
      var vid = document.getElementById('vid');
      
-     jsia.setupVideoCallback(vid, capture, 150);
+     jsia.setupVideoCallback(vid, capture, 10);
 
      function capture()
      {
@@ -23,9 +25,49 @@
 
      function calculateBarcodeLines(coords, imageData)
      {
-	 var startIndex = jsia.xyToIndex(0, coords.y, imageData.width);
+	 var startIndex = jsia.xyToIndex(coords.x, coords.y, imageData.width);
+	 var endIndex = jsia.xyToIndex(coords.x + imageData.width*0.10, coords.y, imageData.width);
 
-	 var threshold = 24;
+	 var pixelMin = 255;
+	 var pixelMax = 0;
+
+	 for(var i = startIndex; i <= endIndex; i+=4)
+	 {
+	     var max = Math.max(imageData.data[i], Math.max(imageData.data[i+1], imageData.data[i+2]));
+	     var min = Math.min(imageData.data[i], Math.min(imageData.data[i+1], imageData.data[i+2]));
+	     if(max > pixelMax)
+	     {
+		 pixelMax = max;
+	     }
+
+	     if(min < pixelMin)
+	     {
+		 pixelMin = min;
+	     }
+	 }
+
+	 var average = (pixelMin + pixelMax)/2;
+	 console.log('average: ' + average);
+
+	 startIndex = jsia.xyToIndex(0, coords.y, imageData.width);
+
+	 for(var i = startIndex; i < startIndex + (imageData.width*4); i += 4)
+	 {
+	     var pixel = Math.min(imageData.data[i], Math.min(imageData.data[i+1], imageData.data[i+2]));
+	     if(pixel > average+10)
+	     {
+		 imageData.data[i] = 255;
+		 imageData.data[i+1] = 255;
+		 imageData.data[i+2] = 255;
+	     }
+	     else
+	     {
+		 imageData.data[i] = 0;
+		 imageData.data[i+1] = 0
+		 imageData.data[i+2] = 0
+	     }
+
+	 }
 
 	 var widths = [];
 	 var currentX = 0;
@@ -39,14 +81,12 @@
 		 continue;
 	     }
 
-	     var pixel = Math.max(imageData.data[i], Math.max(imageData.data[i+1], imageData.data[i+2]));
-	     var lastPixel = Math.max(imageData.data[(i-4)], Math.max(imageData.data[(i-4)+1], imageData.data[(i-4)+2]));
+	     var pixel = imageData.data[i];
+	     var lastPixel = imageData.data[(i-4)];
 
-	     if(Math.abs(pixel - lastPixel) < threshold)
-	     {
-		 currentWidth++;
-	     }
-	     else
+	     currentWidth++;
+
+	     if(pixel != lastPixel)
 	     {
 		 var width = {x: currentX, width:currentWidth};
 		 widths.push(width);
@@ -55,54 +95,116 @@
 	     }
 	 }
 
-	 if(widths.length >= 58)
+	 if(widths.length >= 59)
 	 {
-	     var widthSum = 0;
-	     for(var i = 0; i < widths.length; i++)
+	     widths = removeAboveAverage(widths);
+	     if(typeof widths != 'undefined')
 	     {
-		 widthSum += widths[i].width;
+		 widths = removeAboveAverage(widths);
+	     }
+	     else
+	     {
+		 return null;
 	     }
 
-	     var averageWidth = widthSum / widths.length;
-
-	     var i = widths.length-1;
-	     while(widths.length > 0 && i >= 0)
+	     var gapSum = 0;
+	     
+	     for(var i = 0; i < widths.length-1; i++)
 	     {
-		 if(i > widths.length)
-		 {
-		     i--;
-		 }
-
-		 if(widths[i].width < (averageWidth*4))
-		 {
-		     i--;
-		 }
-		 else
-		 {
-		     widths.splice(i, 1);
-		 }
+		 var expectedX = widths[i].x + widths[i].width + 1;
+		 gapSum += Math.abs(widths[i+1].x - expectedX);
 	     }
 
-	     if(widths.length >= 58)
+	     if(widths.length >= 59)
 	     {
-		 for(var i = 0; i < widths.length; i++)
+		 var indexGroups = [];
+		 var currentIndices = [];
+
+		 for(var i = 0; i < widths.length-1; i++)
 		 {
-		     if(i % 2 == 0)
+		     var expectedX = widths[i].x + widths[i].width;
+		     currentIndices.push(i);
+
+		     if(widths[i+1].x != expectedX)
 		     {
-			 ctx.fillStyle = '#ff0000';
+			 indexGroups.push(currentIndices);
+			 currentIndices = [];
 		     }
-		     else
+		 }
+
+		 indexGroups.push(currentIndices);
+
+		 var selectedGroup = -1;
+
+		 for(var i = 0; i < indexGroups.length; i++)
+		 {
+		     if(indexGroups[i].length >= 59)
 		     {
-			 ctx.fillStyle = '#00ff00';
+			 selectedGroup = i;
 		     }
-		     ctx.fillRect(widths[i].x, coords.y-3, widths[i].width, 6);
 		 }
 		 
-		 return widths;
+		 if(selectedGroup != -1)
+		 {
+		     var newWidths = [];
+		     var indices = indexGroups[selectedGroup];
+
+		     for(var i = 0; i < indices.length; i++)
+		     {
+			 newWidths.push(widths[indices[i]]);
+		     }
+
+		     for(var i = 0; i < newWidths.length; i++)
+		     {
+			 if(i % 2 == 0)
+			 {
+			     ctx.fillStyle = '#ff0000';
+			 }
+			 else
+			 {
+			     ctx.fillStyle = '#00ff00';
+			 }
+			 ctx.fillRect(newWidths[i].x, coords.y-3, newWidths[i].width, 6);
+		     }
+
+		 canvas2.width = canvas.width;
+		 canvas2.height = canvas.height;
+		 var id = ctx.getImageData(0, 0, imageData.width, imageData.height);
+		 ctx2.putImageData(id, 0, 0);
+
+		 
+		     return newWidths;
+		 }
 	     }
  	 }
 
 	 return null;
+     }
+
+     function removeAboveAverage(widths)
+     {
+	 var widthSum = 0;
+	 for(var i = 0; i < widths.length; i++)
+	 {
+	     widthSum += widths[i].width;
+	 }
+	 
+	 var averageWidth = widthSum / widths.length;
+
+	 var i = widths.length-1;
+	 while(widths.length > 0 && i >= 0 && (typeof widths[i] != 'undefined'))
+	 {
+	     if(widths[i].width < (averageWidth*4))
+	     {
+		 i--;
+	     }
+	     else
+	     {
+		 widths.splice(i, 1);
+	     }
+	 }
+
+	 return widths;
      }
 
      function detectBarcode(imageData)
