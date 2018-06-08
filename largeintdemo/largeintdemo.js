1a889b4b54a7b73b1d13f6379705121d94771b7d
Matt Ellen <matt@matthewellen.co.uk>
2018-06-08 19:37:53 +0100

    added charset and script tag

--- largeintdemo/largeintdemo.js
+++ largeintdemo/largeintdemo.js
@@ -1,7 +1,5 @@
-import { LargeInt } from '../LargeInt/largeint.js';
 (function()
  {
-     console.log('skldfhslkdfh');
      var a = new LargeInt('1234');
-     console.log(a);
+     console.log('a:'+a);
  })();
