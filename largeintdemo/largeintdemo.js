cea8f74d972f3238c93861a5c8238528a8b3c3cf
Matt Ellen <matt@matthewellen.co.uk>
2018-06-08 20:37:01 +0100

    added add function

--- largeintdemo/largeintdemo.js
+++ largeintdemo/largeintdemo.js
@@ -1,6 +1,16 @@
 import LargeInt from '../LargeInt/largeint.js';
 (function()
  {
-     var a = new LargeInt('1234');
-     console.log('a:'+a);
+     var btnAdd = document.getElementById('btnAdd');
+     btnAdd.addEventListener('click', function(e)
+			     {
+				 var a = document.getElementById('txtA').value;
+				 var b = document.getElementById('txtB').value;
+
+				 var aInt = new LargeInt(a);
+				 var bInt = new LargeInt(b);
+				 var cInt = aInt.add(bInt);
+
+				 document.getElementById('lblResult').innerHTML = cInt.toString();
+			     });
  })();
